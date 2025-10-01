15e7d41ab998cc56a9bc72fdc5111c932dcbf2fa
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2025-10-01 12:31:16 -0300

    game-page actualizado CSS

--- TPE-PARTE2/js/game-page.js
+++ TPE-PARTE2/js/game-page.js
@@ -2,19 +2,13 @@
 
 let input_comentario = document.getElementById('input-comentario-nuevo')
 
-
-
-
 input_comentario.addEventListener('focus', () => {
 
     let btn_cancelar = document.getElementById('btn-cancelar')
 
     let btn_publicar = document.getElementById('btn-publicar')
 
-    let form_comentario = document.getElementById('form-comentario-nuevo')
-
     input_comentario.classList.add('comentario-nuevo-activo')
-
 
     btn_cancelar.classList.add('mostrar-boton')
     btn_publicar.classList.add('mostrar-boton')
@@ -32,22 +26,70 @@ function cancelar(e, btn_cancelar, btn_publicar, input_comentario) {
     btn_publicar.classList.remove('mostrar-boton')
     btn_cancelar.classList.add('ocultar-boton')
     btn_publicar.classList.add('ocultar-boton')
+    input_comentario.classList.remove('input-comentario-nuevo-null')
+    input_comentario.placeholder = '¿cuál es tu opinion?';
+    input_comentario.classList.remove('input-comentario-nuevo-null')
+
 }
 
 
 function publicar(e, btn_cancelar, btn_publicar, input_comentario) {
     e.preventDefault();
 
-    // CHEQUEAR SI NO ES NULL //
+    if (!input_comentario.value.trim()) 
+       comentario_vacio(input_comentario)
     
-    let mensaje = document.querySelector('.mensaje');
-    let mensaje_inside = document.getElementById('mensaje-inside')
+        else {
 
-    mensaje_inside.innerHTML = 'Comentario publicado.'
+        let mensaje = document.querySelector('.mensaje');
+        let mensaje_inside = document.getElementById('mensaje-inside')
+
+        mensaje_inside.innerHTML = 'Comentario publicado.'
+        setTimeout (() => mensaje.classList.add('mostrar-mensaje'), 500)
+        input_comentario.classList.remove('comentario-nuevo-activo')
+        btn_cancelar.classList.remove('mostrar-boton')
+        btn_publicar.classList.remove('mostrar-boton')
+        input_comentario.classList.remove('input-comentario-nuevo-null')
+
+        let btn_cerrar_mensaje = document.getElementById('btn-cerrar-mensaje')
+        let btn_cancelar_comentario = document.getElementById('btn-cancelar-comentario')
+
+        setTimeout( () => cerrarMensaje(e , mensaje), 15000)
+        btn_cerrar_mensaje.addEventListener('click', () => cerrarMensaje(e, mensaje))
+        btn_cancelar_comentario.addEventListener('click' , () => cancelar_comentario(e, mensaje, mensaje_inside))
+    }
+
+}
+
+function cerrarMensaje(e, mensaje) {
+    e.preventDefault()
+    mensaje.classList.remove('mostrar-mensaje')
+}
+
+function cancelar_comentario(e, mensaje, mensaje_inside){
+    e.preventDefault()
+    mensaje_inside = document.getElementById('mensaje-inside')
+    let btn_deshacer = document.getElementById('mensaje-deshacer')
+
+    btn_deshacer.innerHTML = 'Cancelar'
+    mensaje_inside.innerHTML = 'Comentario deshecho.'
     mensaje.classList.add('mostrar-mensaje')
-    input_comentario.classList.remove('comentario-nuevo-activo')
-    btn_cancelar.classList.remove('mostrar-boton')
-    btn_publicar.classList.remove('mostrar-boton')
+    
+}
 
+function comentario_vacio(){
 
+    let mensaje_error = document.querySelector('.comentario-vacio')
+
+    mensaje_error.classList.add('comentario-vacio-mostrar')
+
+    let btn_cerrar = document.getElementById('btn-cerrar-advertencia')
+
+    btn_cerrar.addEventListener('click' , () => {
+        mensaje_error.classList.remove('comentario-vacio-mostrar')
+    })
+}
+
+function compartir(){
+    
 }
