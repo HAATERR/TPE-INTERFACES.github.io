b179d8a9fb553e5c59763afe1bf26896dfd4066e
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2025-10-05 15:20:33 -0300

    burguer menu parcial

--- TPE-PARTE2/js/game-page.js
+++ TPE-PARTE2/js/game-page.js
@@ -121,3 +121,11 @@ btn_perfil_header.addEventListener('click' , () => {
     let menu_perfil = document.querySelector('.menu-perfil')
     menu_perfil.classList.toggle('menu-perfil-mostrar')
 })
+
+/* MOSTRAR MENU HAMBURGUESA */
+let btn_menu_hamburguesa = document.getElementById('btn-menu-hamburguesa')
+btn_menu_hamburguesa.addEventListener('click' , ()=> {
+    let menu_hamburguesa = document.querySelector('.sidebar')
+    menu_hamburguesa.classList.toggle('sidebar-mostrar')
+    
+})
